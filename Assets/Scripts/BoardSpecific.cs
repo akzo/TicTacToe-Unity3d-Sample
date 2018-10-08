9cecce52365654b3bdc5da82c240300976f03ee9
akzo <anarkazimov98@hotmail.com>
2018-10-08 11:00:55 -0700

    Sample uploaded

---
+++ Assets/Scripts/BoardSpecific.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class BoardSpecific : MonoBehaviour {
+    public bool turn;
+    public int steps;
+
+    private void Start()
+    {
+        GameObject.Find("Button").SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controller : MonoBehaviour {
+    //Textures that correspond to x, o and plain
+    public Texture plain;
+    public Texture cross;
+    public Texture circle;
+    private bool player;
+    private int steps;
+    public GameObject btn;
+    [SerializeField]
+    private Text textField = null;
+    private void Start()
+    {
+        btn = GameObject.Find("Button");
+    }
+
+    void OnMouseDown()
+    {
+        
+        player = this.transform.parent.transform.parent.GetComponent<BoardSpecific>().turn;
+        if (player==true)
+        { 
+            GetComponent<Renderer>().material.mainTexture = cross;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.mainTexture = circle;
+        }
+        
+        this.transform.parent.transform.parent.GetComponent<BoardSpecific>().turn = !player;
+
+        steps = ++this.transform.parent.transform.parent.GetComponent<BoardSpecific>().steps;
+        
+        if(steps == 9)
+        {
+            textField.text = "Game is over. However, I don't have algorithm yet to tell who won....Sorry :(";
+            btn.GetComponentInChildren<Text>().text = "Restart";
+            btn.SetActive(true);
+            //Debug.Log("Game is over. However, I don't have algorithm yet to tell who won....Sorry :(");
+        }
+
+    }
+}
